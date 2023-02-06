79879c93940739b3b0fe01fd95ed3672e9db94a3
Johnatan Paes <johnatanpaes@gmail.com>
2023-02-06 13:04:02 -0300

    conclui a aula 09

--- Python Udemy/03 - iniciando com python/09 - Intro Operadores Aritméticos.py
+++ Python Udemy/03 - iniciando com python/09 - Intro Operadores Aritméticos.py
@@ -15,3 +15,12 @@ print("Divisão: ", divisao)
 
 divisao_inteira = 10//2.2
 print("Divisão inteira: ", divisao_inteira)
+
+exponenciacao = 2 ** 10
+print("Exponenciação", exponenciacao)
+
+modulo = 11 % 2 #RESTO DA DIVISÃO
+print("Módulo: ", modulo)
+
+print(10 % 2 == 0)#impar
+print(11 % 2 == 0)#par
