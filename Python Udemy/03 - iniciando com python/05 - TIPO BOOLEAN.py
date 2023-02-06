3f15036b06e047681a24a3ce0e62a62eb8f5c085
Johnatan Paes <johnatanpaes@gmail.com>
2023-02-06 02:14:32 -0300

    atualização das aulas da 05 a 09

---
+++ Python Udemy/03 - iniciando com python/05 - TIPO BOOLEAN.py
@@ -0,0 +1,12 @@
+# tipo bool
+# ou SIM ou Não
+# possivel questionar com vários operadores
+# O operador '==' é usado como operador lógico
+# que verifica se um operador é igual ao outro
+
+# VERIFICA A IGUALDADE
+print( 10 == 23) #rESPOSTA É FALSA
+
+print(11 == 11)#RESPOTA É VERDADEIRA
+
+print  (type(10==10)) #Retorna o tipo
---
+++ Python Udemy/03 - iniciando com python/09 - Intro Operadores Aritméticos.py
@@ -0,0 +1,17 @@
+# OPERADORES
+
+# SOMA
+adicao = 10+2
+print("adição: ",adicao)
+
+subtracao = 10-2
+print("subitração: ",subtracao)
+
+multiplicacao = 10*2
+print("Mutiplicação: ", multiplicacao)
+
+divisao = 10/2.2
+print("Divisão: ", divisao)
+
+divisao_inteira = 10//2.2
+print("Divisão inteira: ", divisao_inteira)
