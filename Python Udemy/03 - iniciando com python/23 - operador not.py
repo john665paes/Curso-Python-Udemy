78151068af444a6d15db44a37c05700f1c9ce103
john665paes <johnatanpaes@gmail.com>
2023-02-16 11:57:37 -0300

    aula 22

---
+++ Python Udemy/03 - iniciando com python/23 - operador not.py
@@ -0,0 +1,16 @@
+# Operador Lógico
+# and (e) or(ou) not(não)
+# Usado para inverter expressões
+# not True = False
+# not False = True
+
+# EX:
+
+print(not 0) #de negativo para positivo 
+print(not 1) #de positivo para negativo
+
+# verificar se não foi digitado nada
+senha = input('Senha: ')
+
+if not senha:
+    print('Senha incorreta')
