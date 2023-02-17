92e5fcd2305b463cb56e6715010dde085203ab81
john665paes <johnatanpaes@gmail.com>
2023-02-17 11:54:28 -0300

    andamento do curso 27

--- Python Udemy/03 - iniciando com python/26 - formatacao f-string.py
+++ Python Udemy/03 - iniciando com python/26 - formatacao f-string.py
@@ -16,7 +16,17 @@ Conversion flags - !r !s !a
 
 var = 'a b c'
 print(f'{var}')
-print(f'{var:. >10}')
-print(f'{var: <10}')
-print(f'{var: ^9}')
+# Posso preecher espaços no pad com 
+# algum caracter ou com espaço
+print(f'{var:º>10}')
+print(f'{var:&<10}')
+print(f'{var:-^13}')#Para centralizar o print
 print(f'{1000.65125445:.3f}')
+
+# o Sinal de positivo descrito na formatção 
+# indica que o numero deverá ser exibido 
+# como positivo ou negativo
+print(f'{1000.65125445:+,.2f}')
+
+#Hexadecimal 
+print(f'O exadecimal de 1500 é {1500:08X}')
