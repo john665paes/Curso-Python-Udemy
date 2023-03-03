4e5614fa77612d8279446a20e6dae582ef84c163
Johnatan Paes <johnatanpaes@gmail.com>
2023-03-03 15:59:52 -0300

    editei 31

--- Python Udemy/03 - iniciando com python/31 - id.py
+++ Python Udemy/03 - iniciando com python/31 - id.py
@@ -4,4 +4,23 @@ v1 = 'a'
 print(id(v1))
 
 v2 = 'ab'
-print(id(v2))
+print(id(v2),'espaço na memoria')
+
+""" 
+Flag(bandeira - Marca um local)
+None = não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = Identidade
+"""
+condicao = False
+passou_if = None
+
+if condicao:
+    passou_if = True
+    print('Passou no if')
+else:
+    print('não passou')
+
+print(passou_if, passou_if is None)
+print(passou_if, passou_if is not None)
+
