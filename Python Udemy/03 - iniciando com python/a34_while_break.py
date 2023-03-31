a9aebacba0f625681480bd20d98a917fe2c231c9
Johnatan Paes <johnatanpaes@gmail.com>
2023-03-31 17:54:25 -0300

    aula 34

---
+++ Python Udemy/03 - iniciando com python/a34_while_break.py
@@ -0,0 +1,15 @@
+"""
+Repetições pt1
+while (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+
+"""
+condicao = True
+
+while condicao:
+    print(1)
+    print(2)
+    print(3)
+    valor = input("True or False")
+    if valor == False:
+        break
