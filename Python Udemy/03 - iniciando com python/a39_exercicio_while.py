67951123c57a845fc5d2f4391cd5a9741ac46443
Johnatan Paes <johnatanpaes@gmail.com>
2023-04-24 12:59:10 -0300

    aula 39 exercicio com while

---
+++ Python Udemy/03 - iniciando com python/a39_exercicio_while.py
@@ -0,0 +1,14 @@
+"""
+Iterando strings com while
+"""
+nome = 'Johnatan Paes'
+
+nomeTam = len(nome)
+print(nome)
+print(nomeTam)
+cont = 0
+while cont < int(nomeTam):
+    
+    print(nome[cont], cont)
+    cont +=1
+   
