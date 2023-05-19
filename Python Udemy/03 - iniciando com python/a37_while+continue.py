0a15a361d5d036540a33700cb033964b0e640a43
Johnatan Paes <johnatanpaes@gmail.com>
2023-05-19 13:14:11 -0300

    aula37

--- Python Udemy/03 - iniciando com python/a37_while+continue.py
+++ Python Udemy/03 - iniciando com python/a37_while+continue.py
@@ -6,16 +6,20 @@ Loop infinito -> O cod nunca tem fim
 
 """
 cont = 0
-while cont <20:
+while cont <=100:
     cont +=1
 
 
     if cont == 7:
-        print("não vou mostrar 7")
+        print("não vou mostrar, usei um 'CONTINUE' 7")
         continue # O continue pula para a proxima repetição e volta para o inicio do while
-
+    
+    if cont >=27 and cont <=47:
+        # print('pulei do 27 até o 47 no contador')
+        continue
+    # Tanto o break quanto o continue são usados para o while mais proximo dela
     print(cont)
     
-    if cont == 15:
+    if cont == 50:
         break #O break quebra o laço de repetição
 print('Contei até ', cont)
