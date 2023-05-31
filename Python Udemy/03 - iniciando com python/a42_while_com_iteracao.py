abab69fd10c43dcf4db4293f142911d8690b7530
Johnatan Paes <johnatanpaes@gmail.com>
2023-05-31 12:48:45 -0300

    aula 42

---
+++ Python Udemy/03 - iniciando com python/a42_while_com_iteracao.py
@@ -0,0 +1,10 @@
+""" atividade:
+    *Qual a letra aparecceu mais vezes na frase?
+"""
+
+frase = 'O Python é uma linguagem de programação ' \
+    'Multiparadigma '\
+    ' Python foi criado por Guido Van Rossum.' .upper()
+
+print(frase)
+print(frase.count('M'))
