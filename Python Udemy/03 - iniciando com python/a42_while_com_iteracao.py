a2322a4eefeccd85432e4422d63d49847556e00c
Johnatan Paes <johnatanpaes@gmail.com>
2023-06-01 12:54:27 -0300

    aula 42

--- Python Udemy/03 - iniciando com python/a42_while_com_iteracao.py
+++ Python Udemy/03 - iniciando com python/a42_while_com_iteracao.py
@@ -1,10 +1,19 @@
 """ atividade:
-    *Qual a letra aparecceu mais vezes na frase?
+    *Qual a letra apareceu mais vezes na frase?
 """
 
 frase = 'O Python é uma linguagem de programação ' \
     'Multiparadigma '\
-    ' Python foi criado por Guido Van Rossum.' .upper()
+    'Python foi criado por Guido Van Rossum.' 
+    
+    # .upper() upper deixa uma str toda em Maiusculo
 
 print(frase)
-print(frase.count('M'))
+print(frase.count('A')) # o método Count retorna o número de elementos com o valor especificado.
+
+cont = 0
+while cont < len(frase):
+    letra_atual = frase[cont]
+    x_letra_apareceu = frase.count(letra_atual)
+    print(letra_atual, x_letra_apareceu)
+    cont += 1
