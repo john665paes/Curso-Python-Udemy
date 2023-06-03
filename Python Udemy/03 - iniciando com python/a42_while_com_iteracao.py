17384873e1b1b310d9aba1081c9071a52ed5b919
unknown <johnatanpaes@gmail.com>
2023-06-02 23:33:37 -0300

    aula 43 for/in

--- Python Udemy/03 - iniciando com python/a42_while_com_iteracao.py
+++ Python Udemy/03 - iniciando com python/a42_while_com_iteracao.py
@@ -9,11 +9,25 @@ frase = 'O Python é uma linguagem de programação ' \
     # .upper() upper deixa uma str toda em Maiusculo
 
 print(frase)
-print(frase.count('A')) # o método Count retorna o número de elementos com o valor especificado.
+print(frase.count('P')) # o método Count retorna o número de elementos com o valor especificado.
 
 cont = 0
+qtd_apareceu_mais_x = 0
+letra_apareceu_mais_x = ''
 while cont < len(frase):
     letra_atual = frase[cont]
-    x_letra_apareceu = frase.count(letra_atual)
-    print(letra_atual, x_letra_apareceu)
+    quantas_x_letra_apareceu_atual = frase.count(letra_atual)
+
+    if letra_atual == ' ':
+        cont += 1
+        continue
+
+    if qtd_apareceu_mais_x < quantas_x_letra_apareceu_atual:
+        qtd_apareceu_mais_x = quantas_x_letra_apareceu_atual
+        letra_apareceu_mais_x = letra_atual
+
     cont += 1
+
+print(f'A letra que apareceu mais vezes foi '
+      f'"{letra_apareceu_mais_x}" que apareceu '
+      f'{qtd_apareceu_mais_x}x')
