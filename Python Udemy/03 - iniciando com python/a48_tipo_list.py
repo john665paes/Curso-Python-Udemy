51bbe8a06e383d66d0f79c5aa11c9bd8e23ba20f
unknown <johnatanpaes@gmail.com>
2023-06-19 22:59:41 -0300

    Aula a48 Intro a list[]

---
+++ Python Udemy/03 - iniciando com python/a48_tipo_list.py
@@ -0,0 +1,14 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+#         01234
+#        -54321  
+string = 'ABCDE' # 5  caracteres
+lista = ["jOhn", True, 1992, 1.4]
+
+# print(lista[1], type(lista))
+print(lista[-4].upper(), type(lista[-4 ]))
