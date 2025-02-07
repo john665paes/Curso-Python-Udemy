6f8bdf58c103b088593e1cd6fc11e2d9f666dbbc
Johnatan Paes <johnatanpaes@gmail.com>
2025-02-07 12:56:22 -0300

    revisando ate a aula90, agora foi ate a 30

--- Python Udemy/03 - iniciando com python/a12_Exercicio_calculo_imc.py
+++ Python Udemy/03 - iniciando com python/a12_Exercicio_calculo_imc.py
@@ -1,7 +1,18 @@
 nome = 'John'
-alt = 1.75
-peso = 85
+alt = 1.77
+peso = 79
 imc = peso / (alt*alt)
 
 print(f'{nome} tem {alt} de altura')
 print(f'Pesa {peso} e seu IMC é {imc:.2f}')
+
+if imc < 18.5:
+    print(f'{nome} Menor que 18,5: abaixo do peso')
+elif 18.5 <= imc < 25:
+    print(f'{nome} Entre 18,5 e 24,9: peso normal')
+elif 25 <= imc < 30:
+    print(f'{nome} Entre 25 e 29,9: sobrepeso')
+elif 30 <= imc < 40:
+    print(f'{nome} Igual ou acima de 30: obesidade')
+else:
+    print(f'{nome} esta com obesidade morbida')
