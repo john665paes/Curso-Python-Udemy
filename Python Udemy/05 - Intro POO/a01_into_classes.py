2d04477736114d971348aea1a91d62ff27d1e951
Johnatan Paes <100241610+john665paes@users.noreply.github.com>
2025-02-27 14:12:23 -0300

    iniciando POO

---
+++ Python Udemy/05 - Intro POO/a01_into_classes.py
@@ -0,0 +1,21 @@
+# class - Classes são moldes para criar novos objetos
+# As classes geram novos objetos (instâncias) que
+# podem ter seus próprios atributos e métodos.
+# Os objetos gerados pela classe podem usar seus dados
+# internos para realizar várias ações.
+# Por convenção, usamos PascalCase para nomes de
+# classes.
+# string = 'Luiz'  # str
+# print(string.upper())
+# print(isinstance(string, str))
+
+class   Pessoa:
+    def __init__(self, nome, sobrenome, idade):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.idade = idade
+
+
+p1 = Pessoa('Johnatan', 'Paes', 32)  # Instancia da classe pessoa
+
+print(p1.nome, p1.sobrenome, p1.idade, "anos")
