a54a6ee4c6ced73a636d1f7a13cbacfdaef2e952
Johnatan Paes <100241610+john665paes@users.noreply.github.com>
2025-03-10 17:03:25 -0300

    aula 03 e 04

---
+++ Python Udemy/05 - Intro POO/a03 _entendendo_self.py
@@ -0,0 +1,6 @@
+# Essa saula serviu apenas exeplificar o self
+# o self serve para identificar o objeto que estou trabalhando
+#  a classe é o molde
+# A instancia é o objeto que foi criado a partir da classe
+# Uma classe pode gerar várias instâncias, ou seja, objetos.
+# Na classe o self representa o objeto que estou trabalhando, é a própria instância
---
+++ Python Udemy/05 - Intro POO/a04_escopo_metodos_da_classe.py
@@ -0,0 +1,8 @@
+# Escopo da classe e de métodos da classe.
+#  -- Variaveis locais só podem ser utiliadas dentro do escopo da classe
+#  para usar uma variável é precio instaciar o objeto da classe.
+
+# o escopo da classe é onde estou criando coisa,
+# possui atributos de classe
+# possui atributos de instância, usando o self para identificar O OBJETO instanciado
+# 
